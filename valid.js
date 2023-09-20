e0a26a2c227f0c34d07e03824a7f1ff9733694fa
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-20 16:11:47 +0100

    js input commit

---
+++ valid.js
@@ -0,0 +1,8 @@
+// Select all input elements for varification
+const userName = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const phoneNumber = document.getElementById("phoneNumber");
+const gender = document.registration;
+const language = document.getElementById("language");
+const zipcode = document.getElementById("zipcode");
