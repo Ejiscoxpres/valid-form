fe51c6c1ea3dfbfda159e3f6cdb2e5d3735f5cea
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-09-22 15:25:08 +0100

    zipcode js commit

--- valid.js
+++ valid.js
@@ -6,12 +6,12 @@ const phoneNumber = document.getElementById("phoneNumber");
 const gender = document.registration;
 const language = document.getElementById("language");
 const zipcode = document.getElementById("zipcode");
-function formValidation{
+function formValidation(){
     if (userName.value.length < 2 || userName. value. length > 20){
         alert("Name length should be more 2 leeters and less 21 characters");
         userName. focus();
         return false;
-    } if(email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
+    } if(email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{10,20})+$/)){
         alert("please enter a valid email!");
         email.focus;
         return false;
@@ -19,8 +19,8 @@ function formValidation{
         alert("password must contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character, and must be between 10 and 25 characters long.");
         password.focus();
         return false;
-    }if(!phoneNumber.value.match(/^[1-9][0-9]{7,10}$/)){
-        alert("phone number must be 7 and 11 characters long");
+    }if(!phoneNumber.value.match(/^[0-9]{15}$/)){
+        alert("phone number must be at least 15 characters long");
         phoneNumber.focus();
         return false;
     }if(gender.gender.value === ""){
@@ -29,7 +29,11 @@ function formValidation{
     }if(language.value === ""){
         alert("please select your language");
         return false;
-    }
+    }if(!zipcode.value.match(/^[0-9]{6}$/)){
+        alert("please, your zipcode should 6 characters long number!");
+        zipcode.focus();
+        return false;
+    }return true;
     
     
 }
